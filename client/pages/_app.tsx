13d5526f1dd37dbb02093de8110e431bb2b181e0
Arthur Teatree <71916438+R-33@users.noreply.github.com>
2021-06-06 16:32:44 +0930

    Added nextjs/react-query/typescript boilerplate

---
+++ client/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "../styles/globals.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+import React from "react";
+
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
---
+++ client/pages/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+interface indexProps {
+
+}
+
+const index: FC<indexProps> = ({}) => {
+  return (
+    <>
+      <div>index</div>
+    </>
+  );
+}
+
+export default index

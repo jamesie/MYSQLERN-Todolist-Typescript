4d0d56eed1a94e3644935ba6f8a9909c6969b81f
Arthur Teatree <71916438+R-33@users.noreply.github.com>
2021-07-19 19:56:25 +0930

    Created TodoList and child component, child animations and Task Component

--- client/components/home/Task/Task.tsx
+++ client/components/home/Task/Task.tsx
@@ -1,29 +1,117 @@
-import React, { FC } from "react";
+import { Switch } from "@headlessui/react";
+import React, { FC, useState, useEffect } from "react";
+import useSound from "use-sound";
 import OverdueIcon from "./OverdueIcon";
+import task_uncomplete from "../../../sounds/1.mp3";
+import task_complete from "../../../sounds/2.mp3";
+import { queryClient } from "../../../pages/_app";
+import { useMutation } from "react-query";
+import { changeStatus } from "../../../api/task.api";
+import { getTodayDateTime } from "../../../utils/dateParser";
+import { sleep } from "../../../utils/sleep";
 
 export type TaskDate = {
   year: number;
   month: number;
   day: number;
 };
-
 interface TaskProps {
   status: boolean;
   toBeCompletedBy?: string | null;
   taskName: string;
   clientsDate: string;
+  id: number;
+  parentName?: string;
 }
 
 const Task: FC<TaskProps> = ({
+  id,
   status,
   toBeCompletedBy,
   taskName,
   clientsDate,
+  parentName,
 }) => {
+  const [enabled, setEnabled] = useState<boolean>(status);
+
+  useEffect(() => {
+    setEnabled(status);
+  }, [status]);
+  const [complete] = useState(
+    typeof Audio !== "undefined" && new Audio(task_complete)
+  );
+
+  const statusUpdate = useMutation((payload: any) =>
+    changeStatus(payload.status, payload.id)
+  );
+
+  const [uncomplete] = useState(
+    typeof Audio !== "undefined" && new Audio(task_uncomplete)
+  );
+
+  const handleChange = async () => {
+    setEnabled(!enabled)
+    if (status) {
+      uncomplete.volume = 0.2;
+      await uncomplete.play();
+    } else {
+      complete.volume = 0.2;
+      await complete.play();
+    }
+    await sleep(200)
+    statusUpdate.mutateAsync({ status: !status, id });
+    updateCache();
+  };
+
+  useEffect(() => {
+    setEnabled(status);
+  }, [status]);
+
+  const updateCache = () => {
+    queryClient.setQueryData("fetchTodoListsAndTasks", (data: any) => {
+      return data?.map((element) => {
+        element.tasks = element?.tasks?.map((task) => {
+          if (task.id === id) {
+            task.status = !status;
+            return task;
+          }
+          return task;
+        });
+        return element;
+      });
+    });
+  };
+
   return (
-    <div className="flex flex-row w-full bg-white p-2 rounded-sm border-gray-300 border-2 my-1">
-      <div className="">{taskName}</div>
-      <OverdueIcon currentDate={clientsDate} toBeCompletedBy={toBeCompletedBy}/>
+    <div className="flex flex-col sm:flex-row w-full bg-white p-2 rounded-sm border-gray-300 border-2 my-1 text-gray-400">
+      <div className="w-11 mx-auto sm:mx-1">
+        <Switch
+          checked={enabled}
+          onChange={handleChange}
+          className={`${
+            enabled ? "bg-blue-600" : "bg-gray-200"
+          } inline-flex items-center h-6 rounded-full min-w-full focus:outline-none mx-1`}
+        >
+          <div
+            className={`${
+              enabled ? " transform translate-x-6" : " transform translate-x-1"
+            } duration-300 ease-out inline-block w-4 h-4 transform bg-white rounded-full`}
+          />
+        </Switch>
+      </div>
+      <div className="flex flex-row mx-auto sm:mx-1">
+        <div className="font-semibold mr-2 ml-1">
+          {!parentName ? null : `${parentName}:`}
+        </div>
+        {taskName}
+      </div>
+      {toBeCompletedBy ? (
+        <OverdueIcon
+          currentDate={clientsDate}
+          toBeCompletedBy={toBeCompletedBy}
+          className="mx-auto sm:ml-auto sm:mr-0 "
+        />
+      ) : null}
     </div>
   );
 };
--- client/components/home/TodoList.tsx
+++ client/components/home/TodoList.tsx
@@ -3,6 +3,8 @@ import React, { FC, useState } from "react";
 import { GoChevronDown } from "react-icons/go";
 import { RiEdit2Fill } from "react-icons/ri";
 import { BsPlusSquare } from "react-icons/bs";
+import CreateTask from "../TodoListActions/CreateTask";
+import EditTask from "../TodoListActions/EditTask";
 
 interface TodoListProps {
   todoListName: string;
@@ -11,50 +13,88 @@ interface TodoListProps {
 const TodoList: FC<TodoListProps> = ({ todoListName: name, children }) => {
   const [dropDownTasks, setDropDownTasks] = useState(false);
   const [chevron, setChevron] = useState(false);
+  const [showAddTask, setShowAddTask] = useState(false);
+  const [showEditTask, setShowEditTask] = useState(false);
+
+  const childrenArray = React.Children.toArray(children).filter(
+    (element) => {
+      if (!React.isValidElement(element)) return;
+      return element;
+    }
+  );
 
   return (
-    <div className="grid grid-cols-2 gap-y-4 gap-x-3 w-full bg-gray-100 p-3 rounded-sm border-gray-300 border-2 my-2">
-      <h2 className="grid-col-1 font-semibold">{name}</h2>
-      <div className="grid-col-2 flex flex-row">
-        <BsPlusSquare className="ml-auto w-6 h-6 mr-2" />
-        <RiEdit2Fill className="w-6 h-6 mx-2" />
-
-        <GoChevronDown
-          className={`${
-            chevron ? "transform rotate-180" : "transform rotate-0"
-          } outline-none transition-transform duration-500 ease-out w-6 h-6`}
-          onClick={() => {
-            chevron === true ? setChevron(false) : setChevron(true);
-            dropDownTasks === true
-              ? setDropDownTasks(false)
-              : setDropDownTasks(true);
-          }}
-        />
+    <div className="grid grid-rows-1 w-full bg-gray-100 px-3 py-1 rounded-sm border-gray-300 border-2 my-2 gap-y-4 gap-x-3">
+      <div className="grid-row-1 flex flex-row my-1">
+        <h2 className="grid-col-1 font-semibold w-full">{name}</h2>
+        <div className="flex flex-row my-auto ml-4">
+          <button className="focus:outline-none">
+            <BsPlusSquare
+              className="w-6 h-6 mr-2"
+              onClick={() => {
+                setShowAddTask(true);
+              }}
+            />
+          </button>
+          <button className="focus:outline-none">
+            <RiEdit2Fill
+              className="w-6 h-6 mx-2"
+              onClick={() => {
+                setShowEditTask(true);
+              }}
+            />
+          </button>
+          <button className="focus:outline-none"></button>
+          <button className="focus:outline-none">
+            <GoChevronDown
+              className={`${
+                chevron ? "transform rotate-180" : " tranform rotate-0"
+              } duration-500 w-6 h-6`}
+              onClick={() => {
+                chevron === true ? setChevron(false) : setChevron(true);
+                dropDownTasks === true
+                  ? setDropDownTasks(false)
+                  : setDropDownTasks(true);
+              }}
+            />
+          </button>
+        </div>
       </div>
       <Transition
         show={dropDownTasks}
         enter={`transition ease-out ${
-          !children ? "duration-75" : "duration-500"
+          !childrenArray ? "duration-75" : "duration-500"
         }`}
         enterFrom={`transform ${
-          !children ? "opacity-100" : "opacity-0"
+          !childrenArray ? "opacity-100" : "opacity-0"
         } scale-y-0`}
         enterTo="transform opacity-100 scale-y-100"
         leave="transition ease-out duration-75"
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-70"
-        className="grid-col-1 grid-row-1 col-span-2"
+        className="grid-row-2"
       >
-        {!children ? (
+        {childrenArray.length < 1 ? (
           <div className="flex flex-row w-full bg-white p-2 rounded-sm border-gray-300 border-2 my-1">
             <div className="text-center text-red-400 font-semibold text-lg mx-auto">
               No tasks created for this todolist
             </div>
           </div>
         ) : (
-          children
+          childrenArray
         )}
       </Transition>
+      <CreateTask
+        show={showAddTask}
+        onClick={() => {
+          setShowAddTask(false);
+        }}
+      />
+      <EditTask
+        name={name}
+        show={showEditTask}
+        onClick={() => setShowEditTask(!showEditTask)}
+      />
     </div>
   );
 };

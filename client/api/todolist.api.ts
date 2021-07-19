50cd7879c974cb8f85e2112755ff87d8190a3afc
Arthur Teatree <71916438+R-33@users.noreply.github.com>
2021-07-19 19:57:51 +0930

    create todolist api util

--- client/api/todolist.api.ts
+++ client/api/todolist.api.ts
@@ -1,3 +1,4 @@
+import { NextPageContext } from "next";
 import { API_URL } from "./user.api";
 
 export const fetchTodoListsAndTasks = async (ctx) => {
@@ -7,6 +8,43 @@ export const fetchTodoListsAndTasks = async (ctx) => {
     },
     method: "GET",
     credentials: "include",
+  })
+    .then((response) => response.text())
+    .then((res) => JSON.parse(res));
+
+  return res;
+
+  // fetches object with all of users todolists and tasks:
+  // example: {
+  //   0: {
+  //     name: "todolist1"
+  //     tasks: [{
+  //       taskName: "task"
+  //       status: false
+  //       toBeCompletedBy: null
+  //     }]
+  //   }
+  //   1: {
+  //     name: "todolist2"
+  //     tasks: null
+  //   }
+  // }
+};
+
+type createTodoListQueryKey = [
+  string | null,
+  { name: string; ctx: NextPageContext }
+];
+
+export const createTodoList = async (name: string) => {
+  const res = await fetch(API_URL + "/todolist/create", {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    credentials: "include",
+    body: JSON.stringify({ name }),
   })
     .then((response) => response.text())
     .then((res) => JSON.parse(res));
